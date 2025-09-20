b1a85629cd9aa4ccb18d55b85e3744e6727ee2c4
Danan Subramaniam <danan_varo@hotmail.com>
2025-09-21 01:47:54 +0200

    Added new function that allows user to create account with 0 balance but not deposit or withdrawal 0. Commenting done and project done.

--- InputChecker.py
+++ InputChecker.py
@@ -27,6 +27,15 @@ def numberChecker(value):
         raise TypeError("Value must be a number!")
     if value < 0:
         raise ValueError("Value can't be a negative number!")
+    
+def transactionChecker(transaction):
+    '''
+    Checks if input value is a bigger than 0 otherwise will raise a ValueError. The method also checks if the input is a number.
+    '''
+    numberChecker(transaction)
+
+    if (transaction <= 0):
+        raise ValueError("Transaction can't be 0 or negative!")
 
 def balanceChecker(withdrawal,balance):
       
@@ -34,11 +43,13 @@ def balanceChecker(withdrawal,balance):
     Checks if input withdrawal is not greater than balance. If input value is greater than the balance it return a message and False otherwise will it return True. 
     The function also calls the numberChecker. 
     '''
- 
     numberChecker(withdrawal)
+
+    if (withdrawal == 0):
+        raise ValueError("Transaction can't be 0 or negative!")
+        re
 
     if (withdrawal > balance):
         print("Insufficient funds!")
         return False
     return True
-    
--- Mandatory-assignment-1.py
+++ Mandatory-assignment-1.py
@@ -4,48 +4,67 @@ class BankAccount:
 
     def __init__(self, account_holder, balance=0):
 
+        # Checks if name and balance is valid
         InputChecker.nameChecker(account_holder)
         InputChecker.numberChecker(balance)
         
+        # A init method that gets exucted everytime when the class is initiated
         self.account_holder = account_holder
         self.balance = balance
 
+    # Deposit money into balance
     def deposit(self, amount):
 
+        # Checks the input is valid and not 0 or below
         InputChecker.numberChecker(amount)
+        InputChecker.transactionChecker(amount)
 
+        # Updates balance
         self.balance+=amount
 
     def withdraw(self, amount):
-        
+
+        # Uses the balance checker function that checks in amount is a number and 
+        # is bigger than the balance if amount is a invalid number it will break out of the method and not update balance
+        InputChecker.transactionChecker(amount)
+
         if not InputChecker.balanceChecker(amount,self.balance):
             return
 
+        # Updates balance
         self.balance -= amount
 
+    # Prints the objects name and balance
     def account_info(self):
 
         info = f"Account name: {self.account_holder}, Account balance: {self.balance}"
         return info
 
+# Inherits methods from Class BankAccount
 class SavingsAccount(BankAccount):
     
     interest_rate = 0.02
 
+    # When called applies intrest
     def apply_interest(self):
     
         self.balance *= (1 + self.interest_rate) 
 
+# Inherits methods from Class BankAccount
 class CheckingAccount(BankAccount):
 
+    # Fixed transaction fee at 1 dollar
     transaction_fee = 1
 
     def withdraw(self, amount):
 
+        # Calls a function that checks f inputted number is a number
         InputChecker.numberChecker(amount)
+        InputChecker.transactionChecker(amount)
 
+        # Combines the fixed fee and the withdrawal amount
         total = amount+ self.transaction_fee
-        
+
         if not InputChecker.balanceChecker(total,self.balance):
             return
 
@@ -64,8 +83,8 @@ if __name__ == "__main__":
         (2, 2000), # Invalid input due to number "2" in String in input
         ("1",2000), # Invalid input due to number wrapped in String in input
         ("Danan", -2000), # Invalid input  due to a negative input 
-        ("Danan", "hdfhdfj"), # Invalid input due to String in integer/float input
-        ("Danan", 0) # Invalid input due 0
+        ("Danan", "hdfhdfj") # Invalid input due to String in integer/float input
+
     ]
     
     # A loop that loops through the test cases
